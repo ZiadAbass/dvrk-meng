a98ed04bb30b6b8a3e7d25d14a5451af02e5310c
ZiadAbass <el16za@leeds.ac.uk>
2021-02-21 16:22:50 +0200

    implemented interpolation function that extends the number of waypoints according to the desired number of points. interface between dvrk and move_controller is working well now

--- starting/first_traj.py
+++ starting/first_traj.py
@@ -4,7 +4,6 @@ First trial to plan and execute a series of trajectories through a python script
 '''
 TODO
 - Find how to control speed through dvrk package
-- Turn 'interpolate' off in dvrk package
 - How will the acceleration/velocity lists fit in?
 - Interpolate to add more waypoints
 '''
@@ -38,62 +37,154 @@ import numpy as np
 import time
 # import PyKDL
 
-### Initialisations
-# init dvrk objject
-p = dvrk.psm('PSM1')
-# init MC objects
-moveit_commander.roscpp_initialize(sys.argv)
-robot = moveit_commander.RobotCommander()
-scene = moveit_commander.PlanningSceneInterface()
-group_name = "psm_arm"
-group = moveit_commander.MoveGroupCommander(group_name)
-
-### initialise a ROS node
-# rospy.init_node('hamada',anonymous=True)
-
-### move robot to home position
-raw_input('Initialisation completed. Press ENTER to home the PSM')
-p.home()
-time.sleep(1)
-
-### obtain current joint angles from dvrk
-curr_joint_pos = p.get_current_joint_position()
-print('Current joint position:', curr_joint_pos)
-# e.g. array([ 0.  ,  0.  ,  0.12,  0.  ,  0.  ,  0.  ])
-
-### set current angles as starting point in mc space
+# create a RobotState template instead of initialising a new one each time
 # initialise a RobotState object
 start_state = RobotState()
 # define the joint names
 start_state.joint_state.name = ['psm_yaw_joint', 'psm_pitch_joint', 'psm_main_insertion_joint', 'psm_tool_roll_joint', 'psm_tool_pitch_joint', 'psm_tool_yaw_joint'] 
-# define the required start position angles
-start_state.joint_state.position = [0, 0, 0.2, 0, 0, 0]
-# start_state.is_diff = True
-group.set_start_state(start_state)
 
-### set a goal position OR goal joint angles in mc space
-group.set_named_target('zrandom')
+### Initialisations
+# init_dvrk_mc() intiialises the required objects for both the dvrk and the move_commander libraries
+def init_dvrk_mc():
+    # init dvrk objject
+    p = dvrk.psm('PSM1')
+    # init MC objects
+    moveit_commander.roscpp_initialize(sys.argv)
+    robot = moveit_commander.RobotCommander()
+    scene = moveit_commander.PlanningSceneInterface()
+    group_name = "psm_arm"
+    group = moveit_commander.MoveGroupCommander(group_name)
+    return p,group
 
-### plan a traj using mc
-traj_plan = group.plan()
+# set_mc_start_state() uses the RobotState template to set the start position of the robot from the 
+# move_commande's perspective. Takes in a list of 6 joint angles.
+def set_mc_start_state(joint_angles):
+    # define the required start position angles
+    start_state.joint_state.position = joint_angles
+    # start_state.is_diff = True
+    group.set_start_state(start_state)
 
-### convert the plan into a list of joint angles
-# extract the list of `point` objects. Each contains a list of 6 positions, velocities, accelerations, efforts and a duration
-traj_points = traj_plan.joint_trajectory.points
+# set_named_target_and_plan() takes in the name of a saved configuration and plans its path,
+# returning the RobotTrajectory object
+def set_named_target_and_plan(target_name):
+    # set a goal position
+    group.set_named_target(target_name)
+    ### plan a traj using mc
+    traj_plan = group.plan()
+    return traj_plan
 
-# now only extract the positions from each traj point
-pure_positions = []
-for point in traj_points:
-    pure_positions.append(point.positions)
-# pure_positions is now a list. Each item in the list is a list of 6 motor angles.
+# extract_pos_from_plan() takes in the RobotTrajectory object and extracts the list of positional angles from it 
+def extract_pos_from_plan(traj_plan):
+    ### convert the plan into a list of joint angles
+    # extract the list of `point` objects. Each contains a list of 6 positions, velocities, accelerations, efforts and a duration
+    traj_points = traj_plan.joint_trajectory.points
 
-### interpolate the list to have many more waypoints across the trajectory
-# TODO STILL
+    # now only extract the positions from each traj point
+    pure_positions = []
+    for point in traj_points:
+        pure_positions.append(point.positions)
+    # pure_positions is now a list. Each item in the list is a list of 6 motor angles.
+    return pure_positions
 
-### use the dvrk library to follow the list of joint angles and perform the traj
-for pos in pure_positions:
-    p.move_joint(np.array(pos), interpolate = True)
 
-raw_input('Press enter to shutdown')
-### Stop providing power to the arm
-p.shutdown()
+# interpolate() takes in a list of joint configurations and a goal total number of joint configurations. 
+# It interpolates to find additional configurations between each of the existing ones such that the total becomes what is required.
+# Requires: `points`        -> collection of original joint angle lists
+# Requires: `total_points`  -> number of joint angle lists we want in the end
+def interpolate(points, total_points):
+    # if the number of joint angles in the input is the same as the required number, then there's no point interpolating
+    if len(points) >= total_points:
+        return points
+    # find out how many extra interpolated angles we need between each of the existing ones inside `points` such that the total becomes what we need
+    points_per_interval = int(total_points / (len(points)-1))
+    
+    # will get populated with the new extended list of configurations BUT in a different format 
+    # i.e. first element will contain ALL joint angles for the first joint, second element ALL the joint angles for the seconf joint, etc.
+    interpolated_angles = [[],[],[],[],[],[]]
+    # for each joint
+    for current_joint in range(0,6):
+        # all the angles for one specific joint, re-initialised for each new joint.
+        temp_joint_angles = []
+        # loop through all its occurances in the list of configurations
+        for cfg_idx in range(0,len(points)-1):
+            # interpolate points between this joint's value and the same joint's value in the next configuration.
+            interpolated_joint = np.linspace(points[cfg_idx][current_joint],points[cfg_idx+1][current_joint],points_per_interval)
+            temp_joint_angles.extend(interpolated_joint)
+        # update the interpolated_angles with the whole list of angles for that joint and move on
+        interpolated_angles[current_joint] = temp_joint_angles
+
+    # convert the new format containing one item per joint into a format that has one item per configuration (long list of 6 joint angles)
+    extended_points = []
+    config = [[],[],[],[],[],[]]
+    for xx in range(0,len(interpolated_angles[0])):
+        config = [[],[],[],[],[],[]]
+        for ii in range(0,6):
+            config[ii] = interpolated_angles[ii][xx]
+        extended_points.append(config)
+
+    return extended_points
+
+
+if __name__ == '__main__':
+    # initialise the required objects
+    p,group = init_dvrk_mc()
+
+    ### move robot to home position
+    raw_input('Initialisation completed. Press ENTER to home the PSM')
+    p.home()
+    time.sleep(1)
+
+    ### obtain current joint angles from dvrk (list of 6 angles)
+    curr_joint_pos = p.get_current_joint_position()
+    print('Current joint position:', curr_joint_pos)
+
+    # ------------- # GO TO ZRANDOM # ------------- #
+    
+    ### set current angles as starting point in mc space
+    set_mc_start_state(curr_joint_pos)
+
+    ### set a goal position
+    traj_plan = set_named_target_and_plan('zrandom')
+
+    # take only the position angles from the plan
+    pure_positions = extract_pos_from_plan(traj_plan)
+
+    # interpolate to obtain many intermediate points
+    extended_points = interpolate(pure_positions, 7000)
+
+    raw_input('Ready to execute the trajectory. Press Enter.')
+    ### use the dvrk library to follow the list of joint angles and perform the traj
+    for pos in extended_points:
+        p.move_joint(np.array(pos), interpolate = False)
+
+    # ------------- # GO TO ZHOME # ------------- #
+
+    ### set current angles as starting point in mc space
+    set_mc_start_state(curr_joint_pos)
+
+    ### set a goal position
+    traj_plan = set_named_target_and_plan('zhome')
+
+    # take only the position angles from the plan
+    pure_positions = extract_pos_from_plan(traj_plan)
+
+    # interpolate to obtain many intermediate points
+    extended_points = interpolate(pure_positions, 7000)
+
+    raw_input('Ready to execute the trajectory. Press Enter.')
+    ### use the dvrk library to follow the list of joint angles and perform the traj
+    for pos in extended_points:
+        p.move_joint(np.array(pos), interpolate = False)
+
+    raw_input('Press enter to shutdown')
+    ### Stop providing power to the arm
+    p.shutdown()
+
+
+
+
+
+
+
+
+
--- starting/test_bench.py
+++ starting/test_bench.py
@@ -1,62 +1,161 @@
-import sys
-import copy
-import rospy
-import moveit_commander
-import moveit_msgs.msg
-import geometry_msgs.msg
-from math import pi
-from std_msgs.msg import String, Header
-from moveit_commander.conversions import pose_to_list
-from moveit_msgs.msg import RobotState 
-from sensor_msgs.msg import JointState 
+import numpy as np
 
-rospy.init_node('hamada',anonymous=True)
+p1 = [0.16480233975290323, -0.08713767642252303, 0.043108680782781454, 0.3833254045012679, 0.10491328216307251, -0.06873812566123282]
+p2 = [0.13184187180232257, -0.06971014113801842, 0.03448694462622516, 0.30666032360101425, 0.08393062573045801, -0.05499050052898625]
+p3 = [0.09888140385174193, -0.05228260585351382, 0.02586520846966887, 0.22999524270076072, 0.0629479692978435, -0.04124287539673969]
+p4 = [0.06592093590116128, -0.03485507056900921, 0.01724347231311258, 0.15333016180050713, 0.041965312865229004, -0.027495250264493124]
+p5 = [0.03296046795058064, -0.017427535284504605, 0.00862173615655629, 0.07666508090025356, 0.020982656432614502, -0.013747625132246562]
 
-# init MC objects
-moveit_commander.roscpp_initialize(sys.argv)
-robot = moveit_commander.RobotCommander()
-scene = moveit_commander.PlanningSceneInterface()
-group_name = "psm_arm"
-group = moveit_commander.MoveGroupCommander(group_name)
+# list of joint angle lists
+points = [p1,p2,p3,p4,p5]
 
-# print('CURRENT:', group.get_current_joint_values())
+# interpolate() takes in a list of joint configurations and a goal total number of joint configurations. 
+# It interpolates to find additional configurations between each of the existing ones such that the total becomes what is required.
+# Requires: `points`        -> collection of original joint angle lists
+# Requires: `total_points`  -> number of joint angle lists we want in the end
+def interpolate(points, total_points):
+    # if the number of joint angles in the input is the same as the required number, then there's no point interpolating
+    if len(points) >= total_points:
+        return points
+    # find out how many extra interpolated angles we need between each of the existing ones inside `points` such that the total becomes what we need
+    points_per_interval = int(total_points / (len(points)-1))
+    
+    # will get populated with the new extended list of configurations BUT in a different format 
+    # i.e. first element will contain ALL joint angles for the first joint, second element ALL the joint angles for the seconf joint, etc.
+    interpolated_angles = [[],[],[],[],[],[]]
+    # for each joint
+    for current_joint in range(0,6):
+        # all the angles for one specific joint, re-initialised for each new joint.
+        temp_joint_angles = []
+        # loop through all its occurances in the list of configurations
+        for cfg_idx in range(0,len(points)-1):
+            # interpolate points between this joint's value and the same joint's value in the next configuration.
+            interpolated_joint = np.linspace(points[cfg_idx][current_joint],points[cfg_idx+1][current_joint],points_per_interval)
+            temp_joint_angles.extend(interpolated_joint)
+        # update the interpolated_angles with the whole list of angles for that joint and move on
+        interpolated_angles[current_joint] = temp_joint_angles
 
-'''
-# create a joint state object
-joint_state = JointState() 
-# set the joint state parameters
-joint_state.header = Header() 
-joint_state.header.stamp = rospy.Time.now() 
-# joint_state.name = ['psm_rev_joint', 'psm_yaw_joint', 'psm_pitch_joint', 'psm_main_insertion_joint', 'psm_tool_roll_joint', 'psm_tool_pitch_joint', 'psm_tool_yaw_joint']
-joint_state.name = ['psm_yaw_joint', 'psm_pitch_joint', 'psm_main_insertion_joint', 'psm_tool_roll_joint', 'psm_tool_pitch_joint', 'psm_tool_yaw_joint']
-joint_state.position = [0, 0, 0.2, 0, 0, 0] 
+    # convert the new format containing one item per joint into a format that has one item per configuration (long list of 6 joint angles)
+    extended_points = []
+    config = [[],[],[],[],[],[]]
+    for xx in range(0,len(interpolated_angles[0])):
+        config = [[],[],[],[],[],[]]
+        for ii in range(0,6):
+            config[ii] = interpolated_angles[ii][xx]
+        extended_points.append(config)
 
-# create a robot state object that will include the joint state object
-moveit_robot_state = RobotState() 
-moveit_robot_state.joint_state = joint_state 
-# set the start state
-group.set_start_state(moveit_robot_state)
-'''
+    return extended_points
+        
 
-start_state = RobotState()
-start_state.joint_state.name = ['psm_yaw_joint', 'psm_pitch_joint', 'psm_main_insertion_joint', 'psm_tool_roll_joint', 'psm_tool_pitch_joint', 'psm_tool_yaw_joint']
-start_state.joint_state.position = [0, 0, 0.2, 0, 0, 0]
-start_state.is_diff = True
-group.set_start_state(start_state)
+extended_points = interpolate(points,300)
 
-# set names target
-group.set_named_target('zrandom')
-plann = group.plan()
 
-# extract the list of `point` objects. Each contains a list of 6 positions, velocities, accelerations, efforts and a duration
-traj_points = plann.joint_trajectory.points
 
-# now only extract the positions from each traj point
-pure_positions = []
-for point in traj_points:
-    pure_positions.append(point.positions)
-# pure_positions is now a list. Each item in the list is a list of 6 motor angles.
+# ----------------------------------------------------
+# ----------------------------------------------------
+# ----------------------------------------------------
+# ----------------------------------------------------
 
-print('Length:', len(pure_positions))
-print('Potsitions:', pure_positions)
+# get_trajectories() takes in a list of goal coordinates, generates a trajectory 
+# between each point and the next one, and creates a list of trajectories with all of them together.
+# `extend_final` is an optional parameter that adds more points to generate in the final segment of the stroke.
+# This helps the arm reach the end of the stroke accurately rather than finish early
+def get_trajectories_list_from_coords(reachy, goal_coords, predict=False, points=100, extend_final=0, initial_guess=[]):
+    angles_dicts = []
+    repeat_final = False
+    print('Generating', len(goal_coords)-1, 'trajectories for this stroke')
+    for i in range(0, len(goal_coords)-1):
+        # print('Generating trajectory', i, 'out of', len(goal_coords)-1)
+        # add more points to the final segment of the stroke to enforce the arm going to it
+        if i == (len(goal_coords)-3):
+            # print('Adding more points to pre-final')
+            points += int(extend_final/2)
+        if i == (len(goal_coords)-2):
+            # print('Adding more points to final')
+            points += int(extend_final)
+            repeat_final = True         # tells the generate_traj_angles() to add the final coordinate multiple times to the traj. This is to help the arm reach the end of the stroke.
+        H, err = generate_traj_angles(reachy, goal_coords[i], goal_coords[i+1], points, predict=predict, repeat_final=repeat_final, initial_guess=initial_guess)
+        if err:
+            print('Could not obtain traj angles for points', goal_coords[i], goal_coords[i+1])
+            return [], True
+        angles_dicts.append(H)
+    # print('Generated ', len(angles_dicts) ,'trajectories.')
+    return angles_dicts, False
+
+
+# get_focus_coords() takes in a list of coords that make up a stroke.
+# Also takes in the number of focus points needed (i.e. points that will have IK run on them)
+# and returns a list of 3D coords. Their count = focus coords argument.
+def get_focus_coords(stroke, focus_points_in_stroke):
+    if len(stroke) < focus_points_in_stroke:
+        # print('Focus points are more than the number of coordinates in the stroke!\n Will use all points on the stroke as focus points')
+        if len(stroke) > 2:
+            focus_points_in_stroke = len(stroke)
+        else:
+            focus_points_in_stroke = 2      # manually set 2 focus points if the stroke consists of 2 coords to avoid dividing by zero below
+    focus_coords = []
+    # sample some points from the coordinates on the stroke
+    inds = np.linspace(0, len(stroke)-1, focus_points_in_stroke)
+    for ind in inds:
+        focus_coords.append(stroke[int(ind)])
+
+    return focus_coords
+
+# curve_to_traj() takes in a stroke (list of coordinates) and turns it into one trajectory.
+# It also takes in the number of focus points (i.e. points that will have IK run on them)
+# Also takes in the total number of points to generate for the stroke
+def curve_to_traj(reachy, stroke, focus_points_in_stroke, total_points, predict=False, extend_final=0, use_gripper=True, check_bounds=False, initial_guess=[]):
+    # calculate the focus coordinates
+    focus_coords = get_focus_coords(stroke, focus_points_in_stroke)
+
+    # an empty focus_coords list indicates there was an problem with get_focus_coords()
+    if len(focus_coords) == 0:
+        return [], True   # return an empty trajectory to indicate that the points were invalid
+
+    # number of points for each interval to generate for the trajectories
+    points_per_interval = int(total_points / (len(focus_coords)-1))
+
+    # now we have the condensed list of coords
+    # call get_trajectories_list_from_coords(reachy, goal_coords, predict=False) to get list of dictionaries
+    angles_dicts, err = get_trajectories_list_from_coords(reachy, focus_coords, predict=predict, points=points_per_interval, extend_final=extend_final, initial_guess=initial_guess)
+    if err:
+        print("ERROR: Failed to get trajectory from coords:", focus_coords)
+        return [], True
+
+    angles_by_motor = [[], [], [], [], [], [], [], []]
+    # Look through the list and generate one traj from all of them
+    for traj in angles_dicts:
+        for i, motor_angles in enumerate(traj):
+            angles_by_motor[i].extend(traj[motor_angles])
+
+    # create the dictionary to send to the trajectory player
+    one_big_traj = {'left_arm.shoulder_pitch': np.array(angles_by_motor[0]),
+      'left_arm.shoulder_roll': np.array(angles_by_motor[1]),
+      'left_arm.arm_yaw': np.array(angles_by_motor[2]),
+       'left_arm.elbow_pitch': np.array(angles_by_motor[3]),
+        'left_arm.hand.forearm_yaw': np.array(angles_by_motor[4]),
+        'left_arm.hand.wrist_pitch': np.array(angles_by_motor[5]),
+        'left_arm.hand.wrist_roll': np.array(angles_by_motor[6]),
+        'left_arm.hand.gripper': np.array(angles_by_motor[7])}
+
+    # if we are not using a gripper then delete the last entry from the dictionary.
+    if not use_gripper:
+        one_big_traj = remove_gripper_from_dict(one_big_traj)
+
+    return one_big_traj, False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
