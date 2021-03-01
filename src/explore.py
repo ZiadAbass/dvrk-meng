e81048f19f4d1685a766e4f745e969a2b8089693
ZiadAbass <el16za@leeds.ac.uk>
2021-03-01 17:57:41 +0200

    created a diagram showing what is meant by exploration points. Done with the framework for the algorithm represented by the flowchart diagram.

--- src/explore.py
+++ src/explore.py
@@ -7,9 +7,18 @@ Purpose:
 import master as mm
 
 
+# exclude_closest_coord takes in a target coordinate and a list of 'options' coordinates.
+# From the given options, it finds the one closest to the target coordinate and removes it,
+# returning a list of all the remaining options.
+def exclude_closest_coord(target, options):
+    # TODO: Do stuff
+    return options[:-1]
+
 # find_exploration_points() uses the PSM's current position and finds the 4 surrounding coordinates
 # an 'increment' mm away from the current position (in +/- X and +/- Y)
-def find_exploration_points(psm,group,increment):
+# If `exclude_position` is supplied, then only 3 exploration points are returned. 
+#   Of the 4 generate coords, the one closest to the exclude_position is removed.
+def find_exploration_points(psm,group,increment, exclude_position=False):
     # find the arm's position in 3D
     cur_XYZ = mm.read_display_dvrk_pos(psm, verbose=False)
     # convert the increment given from mm to meters
@@ -24,7 +33,11 @@ def find_exploration_points(psm,group,increment):
     down_x = [cur_XYZ[0]-inc_m,cur_XYZ[1],cur_XYZ[2]]
     up_x = [cur_XYZ[0]+inc_m,cur_XYZ[1],cur_XYZ[2]]
     
-    exploration_coords = [up_y, down_y, down_x, up_x]
+    if exclude_position == False:
+        exploration_coords = [up_y, down_y, down_x, up_x]
+    else:
+        # Of down_y, up_y, down_x and up_x, find the closest one to provided coordinate
+        exploration_coords = exclude_closest_coord(target=exclude_position, options=[up_y, down_y, down_x, up_x])
 
     return exploration_coords
 
@@ -32,22 +45,103 @@ def find_exploration_points(psm,group,increment):
 # the location of the relevant blood vessel.
 # Right now, it takes in the simulated result which we want to return.
 # Returns False if the blood vessel cannot be found
+# Returns True if the blood vessel is on the vertical centerline
 # Otherwise, returns the distance of the vessel's center from the vertical centerline of the slice.
 def get_vessel_location(sim_result):
+    # TODO: Do stuff
     return sim_result
 
+# save_slice() saves the ultrasound slice at the current location as well as
+# the current location of the PSM.
+def save_slice():
+    psm_position = mm.read_display_dvrk_pos(psm,verbose=False)
+    print "Ultrasound slice saved, arm is at", psm_position
 
-def start_sequence(psm,group):
-    # find the coordinates of the 4 exploration points
-    exp_coords = find_exploration_points(psm,group,increment=15)
-    # go to all 4 with a fixed vertical orientation
+# explore() loops through a given set of exploration coords.
+# at each coords, it checks to see wherher or not the probe moved closer to the vessel.
+# returns True if the probe moved closer to the vessel.
+# returns False if none of the given coords moved the probe closer to the vessel.
+def explore(psm, exp_coords):
+    # go to all exploration points with a fixed vertical orientation
     for idx, target_coord in enumerate(exp_coords):
         buf = "Press Enter to move to coordinate number %d" % (idx)
         raw_input(buf)
         mm.goto_xyz(psm,target_coord,6000,group,fixed_orientation='vertical')
-    # need to check the vessel's position at each of these 4 coords
-    # as soon as you explore a point at which the vessel is closer to the centerline then stop there.
-    # if 
+        # check where the vessel is in the US slice
+        dv = get_vessel_location(12)
+        # if the vessel has gone out of the FOV or has moved further away
+        if (dv == False) or (dv > dev_Q):
+            # leave if we have looped through all the coords given
+            if idx == len(pp)-1:
+                # return False to indicate none of the exploration coords got the probe closer to the vessel.
+                print "Looped through", len(exp_coords), "exploration coords. None of them got the probe closer to the vessel"
+                return False
+            # otherwise go to the next coord
+            print "Probe has moved further away from the vessel. Continueing to next exploration coord..."
+            continue
+        # if the vessel is on the centerline
+        elif dv == True:
+            save_slice()
+        # we have now moved closer to the vessel, so no need to 
+        # continue looping through the current exploration coords, 
+        # we should generate 3 new ones. 
+        # Return true to indicate a successful exploration
+        return True
+
+def start_sequence(psm,group):
+
+    # check whether or not the vessel is in the field of view
+    dv = get_vessel_location(15)
+    
+    # if the vessel was not detected
+    if dv == False:
+        print "Vessel is outside the probe's FOV, should continue searching"
+        while dv != True:
+            # TODO: Proceed with a search algo that makes PSM scan the are
+            # after each search step, check for the vessel again
+            dv = get_vessel_location(False)
+            print "Still searching..."
+    
+    # if the vessel is on the vertical centerline
+    if dv == True:
+        print "Vessel is on the centerline, saving the slice"
+        save_slice()
+    else:
+        # save the vessel's current deviation from the centerline
+        dev_Q = get_vessel_location()
+    
+    # update pos_Q with the PSM's current position before sending it to exploration coords
+    pos_Q = mm.read_display_dvrk_pos(psm,verbose=False)
+    
+    #  ---------------
+    # TODO: This loop currently happens 10 times.
+    # It should be a while loop using a flag that indicates when it's time to stop
+    for iters in range(0,10):
+        if iters == 0:
+            # if this is the first exploration journey, 
+            # find the coordinates of the 4 exploration points
+            exp_coords = find_exploration_points(psm,group,increment=15)
+        else:
+            # now we have moved closer to the vessel, we only want to find 3 exploration
+            # coordinates, the fourth would bring us back to where we were before.
+            exp_coords = find_exploration_points(psm,group,increment=15, exclude_position=pos_Q)
+
+            # update pos_Q with the PSM's current position before sending it to more exploration coords
+            pos_Q = mm.read_display_dvrk_pos(psm,verbose=False)
+            # save the vessel's current deviation from the centerline
+            dev_Q = get_vessel_location()
+
+        # visit the exploration coords and (hopefully) stop at one that gets the probe closer to the vessel
+        exp_result = explore(psm, exp_coords)
+
+        # if the exploration was successful
+        if exp_result:
+            print "Successful exploration, moved in the right direction"
+        else:
+            print "Unsuccessful exploration, none of the explored coords moved the probe in the right direction"
+            # TODO ?
+
+
 
 if __name__ == '__main__':
     # initialise and home
--- src/test_bench.py
+++ src/test_bench.py
@@ -1,29 +1,5 @@
 
-import master as mm
+pp = [12, 15, 18, 21]
 
-psm,group = mm.init_and_home()
-
-# update the location of the moveit robot
-# mm.update_mc_start_go(psm, group)
-# read full dvrk pose and extract both XYZ and Quaternion
-cur_pose = mm.read_display_dvrk_pose(psm, verbose=False)
-[qx,qy,qz,qw] = cur_pose.M.GetQuaternion()
-[x,y,z] = cur_pose.p
-print "XYZ:\n", [x,y,z]
-print "Quaternion:\n", [qx,qy,qz,qw]
-print "\nPose from MC:\n", group.get_current_pose()
-
-mc_pose = group.get_current_pose()
-[qx,qy,qz,qw] = mc_pose.pose.orientation.x, mc_pose.pose.orientation.y, mc_pose.pose.orientation.z, mc_pose.pose.orientation.w
-
-
-# set pose as target an go
-target = [x,y,z,qx,qy,qz,qw]
-raw_input('Press enter to go to new pose')
-# mm.goto_pose(psm,target,6000,group)
-
-mm.goto_xyz(psm, [x,y+0.05,z],6000,group,fixed_orientation=[qx,qy,qz,qw])
-
-
-# close
-psm.shutdown()
+for iters in range(0,10):
+    print iters
